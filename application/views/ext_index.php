dd129d492a3d1293eaa37cd5fec272f11aefd372
James Roper <vadremix@gmail.com>
2015-06-24 04:02:47 -0400

    changed wording for home page

--- application/views/ext_index.php
+++ application/views/ext_index.php
@@ -1,9 +1,11 @@
 		<div class="jumbotron m-jumbotron">
 			<div class="container">
-				<h1>Share</h1>
-				<p>Sharing makes us human. It connects us with each other and brings us closer together.</p>
-				<h2>Who's borrowing what?</h2>
-				<p>"Was Suzy borrowing <em>Raiders of the Lost Ark</em>? No, wait, it was Jacob, wasn't it?" That's where we come in. Lendiy is a simple way to keep track of your things.</p>
+				<h2>The situation</h2>
+				<p>We let our friends borrow things.</p>
+				<h2>The problem</h2>
+				<p>We forget things. I still don't know who has my copy of <em>Raiders of the Lost Ark</em>.</p>
+				<h2>The solution</h2>
+				<p>Lendiy. It's quick and simple. If you want, it can be advanced too.</p>
 				<p><button class="btn btn-lg btn-primary">Learn more</button></p>
 			</div>
 		</div>
